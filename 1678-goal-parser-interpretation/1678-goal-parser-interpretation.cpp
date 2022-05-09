da986421fa037b7e2a7f19d917e15fee30ef0f7e
Samrat Alam <samratalamshanto710@gmail.com>
2022-05-09 12:45:00 +0600

    Time: 5 ms (17.21%), Space: 6.1 MB (56.67%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -13,11 +13,13 @@ public:
             }
             else if(command[i]=='(' && command[j]==')'){
                 ans += 'o';
+    //if () we need to move pointer 2 steps bcz () contains 2 character
                 i=i+2;
                 j=j+2;
             }
             else if(command[i]=='(' && command[j]=='a'){
                 ans+="al";
+//if (al) we need to move pointer 4 steps bcz (al) contains 4 character
                 i=i+4;
                 j=j+4;
             }
