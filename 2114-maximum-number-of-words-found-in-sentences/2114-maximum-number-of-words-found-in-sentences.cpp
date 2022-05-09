f05b063b063e6dcc3e463e2f08b87d78af9f6bf1
Samrat Alam <samratalamshanto710@gmail.com>
2022-05-09 11:41:26 +0600

    Time: 12 ms (80.36%), Space: 9.8 MB (45.03%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -5,7 +5,8 @@ public:
         
         for(int i=0;i<sentences.size();i++){
             int tmp=0;
-            for(int j=0;j<sentences[i].size();j++){
+// sentences[i].size() gives total character of a sentence. not all words size
+             for(int j=0;j<sentences[i].size();j++){
                 if(j== sentences[i].size()-1 or sentences[i][j]==' '){
                     tmp+=1;
                 }
