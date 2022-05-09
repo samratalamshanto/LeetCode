a8a07c7227d8138077b79cdf8f9623702e99d8f4
Samrat Alam <samratalamshanto710@gmail.com>
2022-05-09 13:24:16 +0600

    Time: 7 ms (85.72%), Space: 15.2 MB (42.77%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
         int len= indices.size();
-        string result=s;
+        string result=s; //same size string are taken
         for(int i=0;i<len;i++){
-            result[indices[i]] =s[i];   
+            result[indices[i]] =s[i];   // then replace them with correct ones
         }
         return result;
         
