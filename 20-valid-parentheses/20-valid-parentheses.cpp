f3d18de14f578bcea463d7ab6d51abd7b94015b5
Samrat Alam <samratalamshanto710@gmail.com>
2022-05-10 11:12:39 +0600

    Time: 2 ms (43.93%), Space: 6.2 MB (79.81%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     bool isValid(string s) {
         int len= s.length();
-        if(len == 1) return false;
+        if(len == 1) return false; //'['
         else{
-        if(s[0]=='}' or s[0]==']' or s[0]==')' ) return false;    
+        if(s[0]=='}' or s[0]==']' or s[0]==')' ) return false; //"]"  
         stack <char> stack;
         
         for(int i=0;i<len;i++){
             
             if(s[i]== '(' or s[i]== '{' or s[i]=='['){
-                stack.push(s[i]);
+                stack.push(s[i]);  
             }
-            else if(!stack.empty())
+            else if(!stack.empty())   //"(){}]["
             {
                 char tmp= s[i];
                 if(stack.top() == '(' &&  tmp == ')' )
