802ec8e71ab3a632b4c06b313d1bc508d0512867
Samrat Alam <samratalamshanto710@gmail.com>
2022-05-10 11:10:54 +0600

    Time: 0 ms (100.00%), Space: 6.3 MB (79.81%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int len= s.length();
+        if(len == 1) return false;
+        else{
+        if(s[0]=='}' or s[0]==']' or s[0]==')' ) return false;    
+        stack <char> stack;
+        
+        for(int i=0;i<len;i++){
+            
+            if(s[i]== '(' or s[i]== '{' or s[i]=='['){
+                stack.push(s[i]);
+            }
+            else if(!stack.empty())
+            {
+                char tmp= s[i];
+                if(stack.top() == '(' &&  tmp == ')' )
+                {
+                    stack.pop();
+                }
+                else if(tmp == '}'  && stack.top() =='{' )
+                {
+                    stack.pop();
+                }
+                else if(tmp == ']' && stack.top() =='[' )
+                {
+                    stack.pop();
+                }
+                else 
+                {return false;}
+            }
+            else return false;
+ 
+        }
+        if(!stack.empty()) {return false;}
+        else {return true;}
+            
+        }
+        
+    }
+};
