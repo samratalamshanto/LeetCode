10efb7ff8902fe4a54a55b09adfa7067b957751b
Samrat Alam <samratalamshanto710@gmail.com>
2022-05-13 09:41:12 +0600

    Time: 7 ms (6.72%), Space: 6.2 MB (40.57%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+      
+        int len=s.length();
+        if(len<=1) return 0;
+        map <char,int> m;     //map
+        int ans=0;
+        
+        for(int i=0;i<len;i++){
+            char c=s[i];
+            m[c]++;         //character counting
+            if(m['R'] == m['L']){     //when character counting, we will check if they equal?
+                ans++;
+            }
+            
+        }
+
+        return ans;
+    }
+};
