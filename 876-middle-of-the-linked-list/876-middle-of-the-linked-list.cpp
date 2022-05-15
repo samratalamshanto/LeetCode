88e34e3f3283957ffb9270ff9402c09b35b594df
Samrat Alam <samratalamshanto710@gmail.com>
2022-05-15 11:58:23 +0600

    Time: 2 ms (56.89%), Space: 7 MB (95.85%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.cpp
@@ -12,17 +12,17 @@ class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
         int count=0;
-        ListNode* head1=head;
+        ListNode* head1=head;    //extra list node
         while(head1->next != nullptr){
-            count++;
+            count++;        //number of nodes
             head1=head1->next;
         }
         if(count%2==0){
-            count=count/2;
+            count=count/2;   //if divisible then second node
 }
         else
         {
-            count=count/2+1;
+            count=count/2+1;   //if divisible then middle node
         }
         
         
