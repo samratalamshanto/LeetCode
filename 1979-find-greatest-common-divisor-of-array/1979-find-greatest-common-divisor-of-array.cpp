a4b2a14d7fdcc5f28d68a3b810dcb8cf66216824
Samrat Alam <samratalamshanto710@gmail.com>
2022-06-25 19:50:21 +0600

    Time: 8 ms (68.03%), Space: 12.5 MB (16.73%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -3,10 +3,10 @@ public:
     int findGCD(vector<int>& nums) {
         int res=-1000000,n=nums.size();
         sort(nums.begin(),nums.end());
-        int x=nums[0];
+        int x=nums[0];  // first find the lowest of two value
         int y=nums[n-1];
-        for(int i=1;i<=nums[0];i++){
-            if(x%i==0 && y%i==0){
+        for(int i=1;i<=nums[0];i++){  //then loop over the limit to lowest value
+            if(x%i==0 && y%i==0){ //if 2 value are divisible by i then set res=i
                 res=i;
             }
         }
