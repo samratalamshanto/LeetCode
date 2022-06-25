4a9c52fa7b5192b79486a08ad9ff78d1abe7f589
Samrat Alam <samratalamshanto710@gmail.com>
2022-06-25 19:37:29 +0600

    Time: 4 ms (44.71%), Space: 5.9 MB (76.04%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int reverse(int x) {
+  long int rem,rev=0;
+
+    if((-1563847412 < x) &&  (x < 1534236469))
+    {
+        int flag=0;
+        if(x<0)
+        {
+            flag=1;
+            x=x*-1;
+        }
+
+
+        while(x>0)
+        {
+
+            rem= x%10;
+            x=x/10;
+            rev= rev*10 + rem;
+        }
+
+        if(flag==1)
+        {
+            rev=-1*rev;
+        }
+    }
+
+
+if(x==-2147483412){
+     int flag=0;
+        if(x<0)
+        {
+            flag=1;
+            x=x*-1;
+        }
+
+
+        while(x>0)
+        {
+
+            rem= x%10;
+            x=x/10;
+            rev= rev*10 + rem;
+        }
+
+        if(flag==1)
+        {
+            rev=-1*rev;
+        }
+}
+
+    return rev;
+
+        
+        
+    }
+};
