5d88c3ca3d0e6609984eaf48b2b82b5e3066de8e
Samrat Alam <samratalamshanto710@gmail.com>
2022-06-29 18:39:15 +0600

    Time: 2110 ms (5.97%), Space: 5.9 MB (66.84%) - LeetHub

--- 507-perfect-number/507-perfect-number.cpp
+++ 507-perfect-number/507-perfect-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checkPerfectNumber(int num) {
         int sum=0;
-        for(int i=1;i<=num/2;i++){
+        for(int i=1;i<=num/2;i++){ //here num/2 otherwise time limit exceed 
             if(num%i==0)
             {
                 sum=sum+i;
