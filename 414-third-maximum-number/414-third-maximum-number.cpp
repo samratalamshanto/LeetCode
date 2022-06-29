1144cb98573abcd368c25911f1c75e24bb76eaba
Samrat Alam <samratalamshanto710@gmail.com>
2022-06-29 18:13:40 +0600

    Time: 14 ms (36.94%), Space: 9.1 MB (78.56%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.cpp
+++ 414-third-maximum-number/414-third-maximum-number.cpp
@@ -6,22 +6,24 @@ public:
         int ans=0;
         int count=1;
  
-            int prev= nums[len-1];
-            for(int i=len-1;i>=0;i--)
+        int prev= nums[len-1]; //set prev value for comparing
+        for(int i=len-1;i>=0;--i)
+        {//first prev and nums[i] same, so --i  
+            if(nums[i]!=prev)
             {
-                if(nums[i]!=prev)
-                {
-                    count++;
-                    prev=nums[i];
-                    
-                }
-                if(count==3)
-                {
-                    return nums[i];
-                }
+                count++;
+                prev=nums[i];  //update prev
+
             }
+            if(count==3)
+            {
+                return nums[i];
+            }
+        }
             
-      return nums[len-1];
+      return nums[len-1]; //otherwise give len-1
+        
+        //1 1 2 here example
         
     }
 };
