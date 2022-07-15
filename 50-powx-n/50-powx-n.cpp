576cacd75edbb0d97fad71993265e6853bc5c88e
Samrat Alam <samratalamshanto710@gmail.com>
2022-07-15 11:48:38 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (43.20%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -5,21 +5,22 @@ public:
     
     double myPow(double x, int n) {
         double sum=1;
-        long long int m=n;
+        long long int m=n; // long long bcz INT_MIN * -1 not in the range of Int
         
         if(n==0) return sum;
         if(x==1.0) return sum;
         if(m<0)
         {
-            m = -m ;
+            m = -m ;          // *********
             x=1/x;
         }
         
        
        
+      //  divide by 2 
+     // if not divide by to means take one out then divide by 2
         
         
-        
-        return (m%2==0)? myPow(x*x, m/2): x*myPow(x*x, m/2);
+        return (m%2==0) ? myPow(x*x, m/2): x*myPow(x*x, m/2);
     }
 };
