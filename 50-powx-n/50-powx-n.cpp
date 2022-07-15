7b9b2578aa608d474812cd915d86fe32f08b43d3
Samrat Alam <samratalamshanto710@gmail.com>
2022-07-15 11:46:42 +0600

    Time: 0 ms (100.00%), Space: 6 MB (43.20%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
+    // void Helper(double &sum,)
+    
+    
     double myPow(double x, int n) {
-        double sum=pow(x,n);
-        return sum;
+        double sum=1;
+        long long int m=n;
+        
+        if(n==0) return sum;
+        if(x==1.0) return sum;
+        if(m<0)
+        {
+            m = -m ;
+            x=1/x;
+        }
+        
+       
+       
+        
+        
+        
+        return (m%2==0)? myPow(x*x, m/2): x*myPow(x*x, m/2);
     }
 };
