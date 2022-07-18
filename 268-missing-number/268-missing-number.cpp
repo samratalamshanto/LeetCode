e2bd2944d866bbd4f2da20e4887c833f23d2e47f
Samrat Alam <samratalamshanto710@gmail.com>
2022-07-18 09:12:38 +0600

    Time: 30 ms (49.66%), Space: 18 MB (18.76%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         int j=0;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n;i++)  //if use <=n then missing last element nums[i] is invlaid, [0,1]: len=2 so nums[2] invalid.
         {
             if(j==nums[i] && j<=n)
             {
