a1c336dee104fbaee0970b97d483d21bcd5fa725
Samrat Alam <samratalamshanto710@gmail.com>
2022-07-21 09:57:50 +0600

    Time: 8 ms (14.28%), Space: 7.6 MB (17.76%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.cpp
@@ -25,14 +25,14 @@ public:
             {
                 ++i;
                 
-                if(i==left-1 && left!=1) {tmpL=tmp;}
+                if(i==left-1 && left!=1) {tmpL=tmp;} //store the start of left portion
                 
                     
-                if(i==right) {tmpR=tmp->next;}
+                if(i==right) {tmpR=tmp->next;}//store the start of the right portion
                 
-                if(i>=left && i<=right){V.push_back(tmp->val);}
-
-
+                //then left + reverser_part + right which will the correct ans
+                
+                if(i>=left && i<=right){V.push_back(tmp->val);} //left and right portion are inserted in the list.
 
                 if(tmp->next != nullptr) 
                 {
@@ -43,10 +43,17 @@ public:
                     break;
                 }
             }
-            reverse(V.begin(),V.end());
-            if(left==1 && left!=right){tmpL->next= new ListNode(0);}
+            
+            reverse(V.begin(),V.end());//reverse
+            
+            if(left==1 && left!=right)
+            {
+                tmpL->next= new ListNode(0);//[3,5]---1,2 where left is the start of linked list. that's why we return head->next
+            } 
             else
-            {tmpL->next=nullptr;}
+            {
+                tmpL->next=nullptr;  //make left's next null and then connect with reverse list  
+            }
 
             for(int i=0;i<V.size();i++)
             {
@@ -57,13 +64,14 @@ public:
                 }
 
             }
-            tmpL->next=tmpR;
+            tmpL->next=tmpR; //now connect wiht the left+reverse list
+            //left + reverser_part + right
         }
        
         
         
         
-    if(left==1 && left!=right) {return head->next;}
+    if(left==1 && left!=right) {return head->next;}//[3,5]---1,2 where left is the start of linked list
     else {return head;}
     }
 };
