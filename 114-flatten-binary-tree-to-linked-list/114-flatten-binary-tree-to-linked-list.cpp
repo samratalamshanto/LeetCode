fa4ffa234f42cd1b6a125feeeecbc6022abb1b4e
Samrat Alam <samratalamshanto710@gmail.com>
2022-07-27 09:35:33 +0600

    Time: 11 ms (51.26%), Space: 12.6 MB (78.06%) - LeetHub

---
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        
+        if(!root) return;
+        
+        if(root->left)
+        {
+            TreeNode* tmp=root->left;
+            TreeNode* tmp2;
+            root->left= nullptr;
+            
+            if(root->right)
+            {
+                tmp2= root->right;
+                root->right = tmp;
+                //here we can not assign tmp->right = tmp2;
+                //frist we need to go last element of the right side of the left part
+                //then we assign nullprt to tmp2
+                while(tmp->right !=nullptr)  //thats the important part
+                {
+                    tmp=tmp->right;
+                }
+                tmp->right = tmp2;
+                
+            }
+            else{
+                root->right = tmp;
+            }
+            
+            
+            // if()
+            // {
+            //     flatten(root->left->right);
+            // }
+            
+        }
+        
+        if(root->right)
+        {
+            flatten(root->right);
+        }
+        
+    }
+};
