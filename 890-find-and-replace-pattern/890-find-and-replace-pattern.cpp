2b53de11c8f30d9ecdf8bb6bfff075dfca8f4d88
Samrat Alam <samratalamshanto710@gmail.com>
2022-07-29 22:50:02 +0600

    Time: 8 ms (44.61%), Space: 8.5 MB (43.97%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+        vector<int> getPattern(string word)
+        {
+            if(word.size()==0)
+            {
+                return {};
+            }
+            
+            vector<int> res;
+            int indx=0;  //get this int value to track new unique char
+            //prev char so same indx
+            //new char new indx
+            
+           
+            unordered_map<char,int> mp;
+
+            for(int i=0;i<word.size();i++)
+            {
+                
+                if(mp.find(word[i]) == mp.end())  //not found
+                {
+                    mp.insert({word[i],indx}); //insert char, and indx
+                    indx++; //increase the indx value
+                
+                   
+                    res.push_back(mp[word[i]]);
+                }
+                else
+                {  //found so get that char's  value and push taht 
+                    res.push_back(mp[word[i]]);
+                    
+                }
+            
+            }
+
+            
+            return res;
+        }
+
+    
+    
+    
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string>ans;
+        vector<int> v = getPattern(pattern);  //get int pattern
+        
+        for(int i=0;i<v.size();i++)
+        {
+            cout<<v[i];
+        }
+        
+        for(int i=0;i<words.size();i++)
+        {
+            vector<int> x= getPattern(words[i]); //get int pattern
+            
+            if(x==v) ans.push_back(words[i]);  //pattern match
+        }
+
+        
+        return ans;
+    }
+};
